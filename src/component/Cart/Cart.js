efbaba2638684df3775d79de46f38a278d580eda
Meshkat Jahan Dipu <meshkatdipu@gmail.com>
2023-01-04 15:06:39 +0600

    sticky

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = ({cart}) => {
     return (
-        <div>
+        <div className='cart'>
             <h2>Order Summary</h2>
             <p>Selected Item: {cart.length}</p>
         </div>
